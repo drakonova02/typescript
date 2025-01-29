56f4d59039a6de10e1ed158834ef2ec58dda0bd0
Drakonova <drakonova02@gmail.com>
2025-01-29 22:34:33 +0200

    hw-7.1/2

--- main.ts
+++ main.ts
@@ -11,20 +11,38 @@ type Admin = {
   role: "admin";
   username: string;
   password: string;
-}
+};
 
 type ExternalUser = {
   oauthToken: string;
+};
+
+function isAdmin(entity: unknown): entity is Admin {
+  return typeof entity === "object" && entity !== null && "role" in entity;
+}
+
+function isUser(entity: unknown): entity is User {
+  return typeof entity === "object" && entity !== null && "username" in entity && "password" in entity;
+}
+
+function isGuest(entity: unknown): entity is Guest {
+  return typeof entity === "object" && entity !== null && "sessionId" in entity;
+}
+
+function isExternalUser(entity: unknown): entity is ExternalUser {
+  return typeof entity === "object" && entity !== null && "oauthToken" in entity;
 }
 
 function login(entity: User | Guest | Admin | ExternalUser): void {
-  if ('role' in entity) {
-    console.log(`Авторизація користувача: з правами ${entity.role}`);
-  } else if ('username' in entity) {
+  if (isAdmin(entity)) {
+    console.log(`Авторизація адміністратора: ${entity.username} з правами ${entity.role}`);
+  } else if (isUser(entity)) {
     console.log(`Авторизація користувача: ${entity.username}`);
-  } else if ('sessionId' in entity) {
+  } else if (isGuest(entity)) {
     console.log(`Авторизація гостя: сесія ${entity.sessionId}`);
-  } else if ('oauthToken:' in entity) {
-    console.log(`Авторизація охоронця: за токеном ${entity.oauthToken}`);
+  } else if (isExternalUser(entity)) {
+    console.log(`Авторизація зовнішнього користувача за токеном ${entity.oauthToken}`);
+  } else {
+    console.log("Невідомий тип користувача");
   }
 }
