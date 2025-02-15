b416598adabccd0ca7358991d861180702be1e58
Drakonova <drakonova02@gmail.com>
2025-02-15 22:46:29 +0200

    hw-11.1

--- main.ts
+++ main.ts
@@ -1,81 +1,27 @@
 // Task 1
 
-function sortArray<T>(arr: T[], compareFn: (a: T, b: T) => number): T[];
-function sortArray<T, K extends keyof T>(arr: T[], key: K): T[];
-function sortArray<T, K extends keyof T>(
-  arr: T[], 
-  param: ((a: T, b: T) => number) | K
-): T[] {
-  if (typeof param === "function") {
-    return [...arr].sort(param);
-  } else {
-    return [...arr].sort((a, b) => {
-      if (a[param] < b[param]) return -1;
-      if (a[param] > b[param]) return 1;
-      return 0;
-    });
-  }
+type DeepMutable<T> = {
+  -readonly [P in keyof T]: T[P] extends object ? DeepMutable<T[P]> : T[P];
 }
-
-const numbers = [5, 3, 8, 1];
-console.log(sortArray(numbers, (a, b) => a - b));
-
-const users = [
-  { name: "Alice", age: 25 },
-  { name: "Bob", age: 22 },
-  { name: "Charlie", age: 30 }
-];
-console.log(sortArray(users, "age"));
-
 
 // Task 2
 
-type DeepReadonly<T> = {
-  +readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
+type PickByValueType<T, ValueType> = {
+  [P in keyof T as T[P] extends ValueType ? P : never]: T[P];
 }
-
 
 // Task 3
 
-type DeepRequireReadonly<T> = {
-  +readonly [P in keyof T]-?: T[P] extends object ? DeepRequireReadonly<T[P]> : T[P];
+type OmitByValueType<T, ValueType> = {
+  [P in keyof T as T[P] extends ValueType ? never : P]: T[P];
 }
-
 
 // Task 4
 
-type PartialByKeys<T, K extends keyof T> = {
-  [P in keyof T]: T[P];
-} & { [P in K]?: T[P] };
+type CustomReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : never;
+
 
 // Task 5
 
-type ReadonlyByKeys<T, K extends keyof T> = {
-  [P in keyof T]: T[P];
-} & { +readonly [P in K]: T[P] };
-
-// Task 6
-
-type MutableByKeys<T, K extends keyof T> = {
-  [P in keyof T]: T[P];
-} & { -readonly [P in K]: T[P] };
-
-
-// Task 7
-
-type UpperCaseKeys<T> = {
-  [P in keyof T as Uppercase<string & P>]: T[P];
-}
-
-// Task 8
-
-type CustomPropertyDescriptor<T> = {
-  value: T;
-  writable: boolean;
-  enumerable: boolean;
-  configurable: boolean;
-};
-
-type ObjectToPropertyDescriptor<T> = {
-  [P in keyof T]: CustomPropertyDescriptor<T[P]>;
-};
+type ExtendedCustomReturnType<T extends (...args: any) => any> = 
+  T extends (...args: infer A) => infer R ? [R, A] : never;
